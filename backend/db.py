cfb14d137d2e0bca5b2265ef88904dd928ac2b05
Johnathan Maytliss <jmpstillsart@gmail.com>
2022-09-08 14:44:08 +0300

    mongo connection

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,25 @@
-
+import os
 from pymongo import MongoClient
 
-import consts
+class MongoDB(object):
+
+    def __init__(self):
+         super().__init__()
+         self.client     = None
+         self.collection = None
+
+    DB_USER       = os.environ.get("MONGO_USER")
+    DB_PASS       = os.environ.get("MONGO_PASS")
+    DB_CONNECTION = f"mongodb+srv://{DB_USER}:{DB_PASS}@mongocluster.v8xuwmi.mongodb.net/?retryWrites=true&w=majority"
+    DB_PORT       = 27017
+
+    def connect(self):
+        self.client = MongoClient(self.DB_CONNECTION, self.DB_PORT, maxPoolSize=50)
 
 if __name__ == '__main__':
-    client = MongoClient(consts.DB_URL, consts.DB_PORT, maxPoolSize=50)
-    print(client)
-    db = client["main_db"]
+    db = MongoDB()
+    db.connect()
+    db = db.client["main_db"]
     collection = db['users']
     cursor = collection.find({})
     for document in cursor:
