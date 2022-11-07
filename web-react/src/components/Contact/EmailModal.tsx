424bc582178095bba56fee077ad25b7000f84954
Johnathan Maytliss <jmpstillsart@gmail.com>
2022-11-07 13:58:56 +0200

    Contact EmailModal to generic

--- web-react/src/components/Contact/EmailModal.tsx
+++ web-react/src/components/Contact/EmailModal.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import {
-  Typography,
-  TextField,
-  Backdrop,
-  Modal,
-  Fade,
-  Stack,
-} from "@mui/material";
+import { Typography, TextField, Stack } from "@mui/material";
 import { Email } from "@mui/icons-material";
 import { LinkButton } from "../LinkButton";
+import { Modal } from "../Modal";
 
 const ModalStackStyle = {
   position: "absolute" as "absolute",
@@ -17,7 +11,7 @@ const ModalStackStyle = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  minHeight: "50%",
+  minHeight: 500,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -27,9 +21,7 @@ const ModalStackStyle = {
 const EmailModal = ({ email }: { email: string }) => {
   const [bodyValue, SetBodyValue] = React.useState("");
   const [subjValue, SetSubjValue] = React.useState("");
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [emailModalOpen, setEmailModalOpen] = React.useState(false);
 
   const handleBodyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     SetBodyValue(event.target.value);
@@ -41,61 +33,49 @@ const EmailModal = ({ email }: { email: string }) => {
 
   return (
     <>
-      <LinkButton onClick={handleOpen} icon={<Email />}>
+      <LinkButton onClick={() => setEmailModalOpen(true)} icon={<Email />}>
         Email Me
       </LinkButton>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Stack component="form" sx={ModalStackStyle}>
-            <Typography variant="h4" align="center" color="text.primary">
-              Hey,
-            </Typography>
-            <Typography variant="h5" align="center" color="text.primary">
-              I would love to hear from u!
-            </Typography>
-            <TextField
-              id="standard-textarea"
-              label="Please enter subject"
-              autoFocus
-              multiline
-              fullWidth
-              variant="standard"
-              minRows={1}
-              maxRows={2}
-              value={subjValue}
-              onChange={handleSubjChange}
-            />
-            <TextField
-              id="outlined-multiline-flexible"
-              label="Please enter your message"
-              autoFocus
-              multiline
-              fullWidth
-              minRows={5}
-              maxRows={12}
-              value={bodyValue}
-              onChange={handleBodyChange}
-            />
-            <LinkButton
-              href={`mailto:${email}?subject=${subjValue}&body=${bodyValue}`}
-              icon={<Email />}
-              disabled={!subjValue || !bodyValue}
-              onClick={handleClose}
-            >
-              Send Email
-            </LinkButton>
-          </Stack>
-        </Fade>
+      <Modal shouldOpen={emailModalOpen} setModalOpen={setEmailModalOpen}>
+        <Stack component="form" sx={ModalStackStyle}>
+          <Typography variant="h4" align="center" color="text.primary">
+            Hey,
+          </Typography>
+          <Typography variant="h5" align="center" color="text.primary">
+            I would love to hear from u!
+          </Typography>
+          <TextField
+            id="standard-textarea"
+            label="Please enter subject"
+            autoFocus
+            multiline
+            fullWidth
+            variant="standard"
+            minRows={1}
+            maxRows={2}
+            value={subjValue}
+            onChange={handleSubjChange}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Please enter your message"
+            autoFocus
+            multiline
+            fullWidth
+            minRows={5}
+            maxRows={12}
+            value={bodyValue}
+            onChange={handleBodyChange}
+          />
+          <LinkButton
+            href={`mailto:${email}?subject=${subjValue}&body=${bodyValue}`}
+            icon={<Email />}
+            disabled={!subjValue || !bodyValue}
+            onClick={() => setEmailModalOpen(false)}
+          >
+            Send Email
+          </LinkButton>
+        </Stack>
       </Modal>
     </>
   );
